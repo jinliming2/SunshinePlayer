0cffe130851ef955a1036e38274d8c9e37db4fee
金黎明 <jinliming2@gmail.com>
2016-04-11 15:56:05 +0800

    引用BASS音频库

--- SunshinePlayer/Config.cs
+++ SunshinePlayer/Config.cs
@@ -1,5 +1,11 @@
 ﻿namespace SunshinePlayer {
+    /// <summary>
+    /// 配置类
+    /// </summary>
     class Config {
+        /// <summary>
+        /// 是否显示播放列表
+        /// </summary>
         public static bool playListVisible = false;
     }
 }
---
+++ SunshinePlayer/Player.cs
@@ -0,0 +1,7 @@
+﻿namespace SunshinePlayer {
+    /// <summary>
+    /// 音乐播放类
+    /// </summary>
+    class Player {
+    }
+}
