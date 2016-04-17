2ecf56f3bf9b36ae616955753fde94b811ccb939
金黎明 <jinliming2@gmail.com>
2016-04-17 20:52:22 +0800

    Bass Net 库注册信息

---
+++ SunshinePlayer/BassNetRegistration.cs
@@ -0,0 +1,15 @@
+﻿namespace SunshinePlayer {
+    /// <summary>
+    /// Bass Net 库注册信息
+    /// </summary>
+    class BassNetRegistration {
+        /// <summary>
+        /// 电子邮箱
+        /// </summary>
+        public readonly static string eMail = null;
+        /// <summary>
+        /// 激活码
+        /// </summary>
+        public readonly static string registrationKey = null;
+    }
+}
