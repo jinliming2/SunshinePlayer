1374068ca84be4ba0dacd5a2a521dcb42415fcca
金黎明 <jinliming2@gmail.com>
2016-04-17 22:22:33 +0800

    音乐ID3V1信息

---
+++ SunshinePlayer/MusicID3.cs
@@ -0,0 +1,35 @@
+﻿namespace SunshinePlayer {
+    /// <summary>
+    /// 音乐ID3V1信息
+    /// </summary>
+    struct MusicID3 {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title;  //max. 30 chars
+        /// <summary>
+        /// 艺术家
+        /// </summary>
+        public string artist;  //max. 30 chars
+        /// <summary>
+        /// 专辑
+        /// </summary>
+        public string album;  //max. 30 chars
+        /// <summary>
+        /// 年份
+        /// </summary>
+        public string year;  //yyyy
+        /// <summary>
+        /// 评论
+        /// </summary>
+        public string comment;  //max. 28 chars
+        /// <summary>
+        /// 标识码
+        /// </summary>
+        public string genre_id;
+        /// <summary>
+        /// 轨道
+        /// </summary>
+        public string track;  //0-255
+    }
+}
