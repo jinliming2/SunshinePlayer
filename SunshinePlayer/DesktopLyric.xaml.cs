fbabaf41ef2a67f9985d7ad32e49b3818f86a320
金黎明 <jinliming2@gmail.com>
2016-06-27 12:54:49 +0800

    创建桌面歌词窗口

---
+++ SunshinePlayer/DesktopLyric.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SunshinePlayer {
+    /// <summary>
+    /// DesktopLyric.xaml 的交互逻辑
+    /// </summary>
+    public partial class DesktopLyric : Window {
+        public DesktopLyric() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
